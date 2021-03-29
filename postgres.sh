5414fdd0eac53a688d8ff3c063eecf438c28e4a7
agusriga <agusriga@RTX3090.localdomain>
2021-03-30 00:46:38 +0200

    sudo added to few strings in postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+#PostgreSQL Apt reference: https://www.postgresql.org/download/linux/ubuntu/
+
+# Create the file repository configuration:
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 # Update the package lists:
-apt-get update
+sudo apt-get update
 
 # Install PostgreSQL 8.4.
-apt-get install -y postgresql-8.4
+sudo apt-get install -y postgresql-8.4
